3c9176e0c627b17b468fbcd3ea2bc489c38c7fb1
Brian Wlazlo <brianwlazlo7@gmail.com>
2021-02-15 12:03:19 -0600

    experiments with link hover effects

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+const $latestWork = $('.latest-work-grp');
+
+$latestWork.waypoint(function (direction) {
+  if (direction == 'down') {
+    $latestWork.addClass('js-project-grp-animate');
+  } else {
+    $latestWork.removeClass('js-project-grp-animate');
+  }
+  console.log('waypoint!')
+}, {offset: '50%'});
+
+*/
